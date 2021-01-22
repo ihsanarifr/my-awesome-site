9a6ef41691e96f614756265aaa8aefde84114854
ihsanarif <rahman.ihsan@tokopedia.com>
2021-01-22 11:55:44 +0700

    first commit

---
+++ strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd4265be482ffc55782e1e2a386e57b36'),
+    },
+  },
+});
---
+++ strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1a117bd8-433a-4322-a0b1-32344d7fc8d5'
+};
